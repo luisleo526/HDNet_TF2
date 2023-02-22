0140c2f53efc92c9d4e6a1b59e78ee4c8df1fc12
leo <luisleo526@yahoo.com>
2023-02-22 14:46:09 +0800

    add exception catch

--- preprocessing/SSL_Image.py
+++ preprocessing/SSL_Image.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+sys.path.append("/detectron2/projects/DensePose")
 from argparse import ArgumentParser
 from pathlib import Path
 
@@ -58,7 +61,9 @@ def resize_image(img, size=(28, 28)):
 
 
 if __name__ == '__main__':
+
     args = parse_args()
+    args.img_size = (args.img_size, args.img_size)
 
     data = torch.load(args.pickle_path)
 
@@ -68,34 +73,41 @@ if __name__ == '__main__':
     Path(os.path.join(args.output, 'mask')).mkdir(parents=True, exist_ok=True)
 
     for info in data:
-        original_img = imageio.imread(info['file_name'])
-        H, W, _ = original_img.shape
-        original_img = np.array(original_img)
 
-        u, v = (info['pred_densepose'][0].uv * 255).to(torch.uint8).cpu().numpy()
-        i = (info['pred_densepose'][0].labels).to(torch.uint8).cpu().numpy()
-        single_mask = ((i != 0) * 255).astype(np.uint8)
+        try:
+            original_img = imageio.imread(info['file_name'])
+            H, W, _ = original_img.shape
+            original_img = np.array(original_img)
 
-        pad_size = padding_size(info['pred_boxes_XYXY'][0].to(torch.int).tolist(), (H, W), u.shape)
+            u, v = (info['pred_densepose'][0].uv * 255).to(torch.uint8).cpu().numpy()
+            i = (info['pred_densepose'][0].labels).to(torch.uint8).cpu().numpy()
+            single_mask = ((i != 0) * 255).astype(np.uint8)
 
-        u = np.pad(u, pad_size, 'constant', constant_values=((0, 0), (0, 0)))
-        v = np.pad(v, pad_size, 'constant', constant_values=((0, 0), (0, 0)))
-        i = np.pad(i, pad_size, 'constant', constant_values=((0, 0), (0, 0)))
-        single_mask = np.pad(single_mask, pad_size, 'constant', constant_values=((0, 0), (0, 0)))
+            pad_size = padding_size(info['pred_boxes_XYXY'][0].to(torch.int).tolist(), (H, W), u.shape)
 
-        densepose = np.transpose(np.stack((v, u, i)), (1, 2, 0))
-        mask = np.transpose(np.stack((single_mask, single_mask, single_mask)), (1, 2, 0))
+            u = np.pad(u, pad_size, 'constant', constant_values=((0, 0), (0, 0)))
+            v = np.pad(v, pad_size, 'constant', constant_values=((0, 0), (0, 0)))
+            i = np.pad(i, pad_size, 'constant', constant_values=((0, 0), (0, 0)))
+            single_mask = np.pad(single_mask, pad_size, 'constant', constant_values=((0, 0), (0, 0)))
 
-        mask_image = np.copy(original_img)
-        mask_image[mask == 0] = 255
+            densepose = np.transpose(np.stack((v, u, i)), (1, 2, 0))
+            mask = np.transpose(np.stack((single_mask, single_mask, single_mask)), (1, 2, 0))
 
-        basename = os.path.basename(info["file_name"])
+            mask_image = np.copy(original_img)
+            mask_image[mask == 0] = 255
 
-        Image.fromarray(resize_image(densepose, args.img_size), "RGB").save(
-            os.path.join(args.output, 'densepose', basename))
-        Image.fromarray(resize_image(original_img, args.img_size), "RGB").save(
-            os.path.join(args.output, 'color', basename))
-        Image.fromarray(resize_image(mask_image, args.img_size), "RGB").save(
-            os.path.join(args.output, 'color_WO_bg', basename))
-        Image.fromarray(resize_image(mask, args.img_size), "RGB").save(
-            os.path.join(args.output, 'mask', basename))
+            basename = os.path.basename(info["file_name"])
+
+            Image.fromarray(resize_image(densepose, args.img_size), "RGB").save(
+                os.path.join(args.output, 'densepose', basename))
+            Image.fromarray(resize_image(original_img, args.img_size), "RGB").save(
+                os.path.join(args.output, 'color', basename))
+            Image.fromarray(resize_image(mask_image, args.img_size), "RGB").save(
+                os.path.join(args.output, 'color_WO_bg', basename))
+            Image.fromarray(resize_image(mask, args.img_size), "RGB").save(
+                os.path.join(args.output, 'mask', basename))
+
+        except Exception as e:
+
+            print(str(e))
+            print(info['file_name'])
